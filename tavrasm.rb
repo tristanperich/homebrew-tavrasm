166a176fb96e2ab624bb26fbc260b02722c3badf
tristanperich <mail@tristanperich.com>
2024-12-03 21:02:07 -0500

    Update target file

--- tavrasm.rb
+++ tavrasm.rb
@@ -1,8 +1,8 @@
 class Tavrasm < Formula
   desc "AVR assembler"
   homepage "https://github.com/tristanperich/tavrasm"
-  url "https://github.com/tristanperich/tavrasm/archive/refs/tags/v1.2.6.tar.gz"
-  sha256 "9023a5c9795782735aaa195f3d7d37ad094a42f02edcb4a39dfb903120ab97b9"
+  url "https://github.com/tristanperich/tavrasm/archive/refs/tags/v1.2.7.tar.gz"
+  sha256 "dff25f0c96104f3a8260cd1cb31b2cecf8ca5e75611088a4ff8ecc84814bc0c4"
   license "gpl-2.0"
 
   def install
